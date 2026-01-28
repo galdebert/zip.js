ce084c838ab01a014a02b273ebb860d77019c766
Guillaume Aldebert <galdebert@gmail.com>
2026-01-28 16:18:09 +0100

    fix cfgs order

--- tests-perf/all/test-parallel-perf.js
+++ tests-perf/all/test-parallel-perf.js
@@ -23,73 +23,75 @@ export { test };
  */
 
 async function test() {
-	/** @type {PerfConfig} */
-	const baseCfg = {
-		// in out
-		entrySize: 1024 * 1024 * 20,
-		entryCount: 20,
-		inputType: undefined, // "Uint8Array" | "Blob",  undefined means "Uint8Array"
-		outputType: undefined, // "Uint8Array" | "Blob",  undefined means "Uint8Array"
+  /** @type {PerfConfig} */
+  const baseCfg = {
+    // in out
+    entrySize: 1024 * 1024 * 20,
+    entryCount: 20,
+    inputType: undefined, // "Uint8Array" | "Blob",  undefined means "Uint8Array"
+    outputType: undefined, // "Uint8Array" | "Blob",  undefined means "Uint8Array"
 
-		// configure
-		useWebWorkers: undefined,
-		maxWorkers: undefined,
-		useCompressionStream: undefined,
-		chunkSize: undefined,
+    // configure
+    useWebWorkers: undefined,
+    maxWorkers: undefined,
+    useCompressionStream: undefined,
+    chunkSize: undefined,
 
-		// ZipWriter ctr
-		level: 6,
-		keepOrder: undefined,
-		bufferedWrite: undefined,
-	};
-	
-	/** @type {PerfConfig[]} */
-	const cfgs = [
-		{ ...baseCfg, level: 1, useWebWorkers: true  },
-		{ ...baseCfg, level: 1, useWebWorkers: false },
-		{ ...baseCfg, level: 6, useWebWorkers: false },
-		{ ...baseCfg, level: 6, useWebWorkers: true  },
-		//{ ...baseCfg, level: 6, useWebWorkers: true, useCompressionStream: false },
-	];
+    // ZipWriter ctr
+    level: 6,
+    keepOrder: undefined,
+    bufferedWrite: undefined,
+  };
 
-	for (const cfg of cfgs) {
-		/** @type {Input[]} */
-		const inputs = [];
-		for (let i = 0; i < cfg.entryCount; i++) {
-			const byteSize = cfg.entrySize;
-			inputs.push({ name: `entry #${i + 1}`, data: getBlobOrU8(byteSize, cfg.inputType) });
-		}
+  /** @type {PerfConfig[]} */
+  const cfgs = [
+    { ...baseCfg, level: 1, useWebWorkers: true },
+    { ...baseCfg, level: 1, useWebWorkers: false },
+    { ...baseCfg, level: 6, useWebWorkers: true },
+    { ...baseCfg, level: 6, useWebWorkers: false },
+    //{ ...baseCfg, level: 6, useWebWorkers: true, useCompressionStream: false },
+  ];
 
-		const zip_t0 = Date.now();
-		const zipped = await zip(cfg, inputs);
-		const zip_dt = Date.now() - zip_t0;
+  for (const cfg of cfgs) {
+    /** @type {Input[]} */
+    const inputs = [];
+    for (let i = 0; i < cfg.entryCount; i++) {
+      const byteSize = cfg.entrySize;
+      inputs.push({
+        name: `entry #${i + 1}`,
+        data: getBlobOrU8(byteSize, cfg.inputType),
+      });
+    }
 
-		let zippedByteLength = 0;
-		if (zipped instanceof Uint8Array) {
-			zippedByteLength = zipped.byteLength;
-		}
+    const zip_t0 = Date.now();
+    const zipped = await zip(cfg, inputs);
+    const zip_dt = Date.now() - zip_t0;
 
-		const unzip_t0 = Date.now();
-		const outputs = await unzip(cfg, zipped);
-		const unzip_dt = Date.now() - unzip_t0;
+    let zippedByteLength = 0;
+    if (zipped instanceof Uint8Array) {
+      zippedByteLength = zipped.byteLength;
+    }
 
-		if (outputs.length != inputs.length) {
-			throw new Error("Unzipped entry count mismatch");
-		}
+    const unzip_t0 = Date.now();
+    const outputs = await unzip(cfg, zipped);
+    const unzip_dt = Date.now() - unzip_t0;
 
-		const zip_sec = (zip_dt / 1000).toFixed(2);
-		const unzip_sec = (unzip_dt / 1000).toFixed(2);
-		const zippedMiB = (zippedByteLength / (1024 * 1024)).toFixed(2);
-		const pov = {
-			cfg,
-			results: { zip_sec, unzip_sec, zippedMiB }
-		};
-		
-		// eslint-disable-next-line no-console
-		console.log(JSON.stringify(pov, null, 2));
-	}
+    if (outputs.length != inputs.length) {
+      throw new Error("Unzipped entry count mismatch");
+    }
+
+    const zip_sec = (zip_dt / 1000).toFixed(2);
+    const unzip_sec = (unzip_dt / 1000).toFixed(2);
+    const zippedMiB = (zippedByteLength / (1024 * 1024)).toFixed(2);
+    const pov = {
+      cfg,
+      results: { zip_sec, unzip_sec, zippedMiB },
+    };
+
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(pov, null, 2));
+  }
 }
-
 
 //--------------------------------------------------------------------------------------------------
 /**
@@ -97,20 +99,24 @@ async function test() {
  * @param {Input[]} inputs
  */
 async function zip(cfg, inputs) {
-	configure(cfg);
+  configure(cfg);
 
-	const zipWriter = new zipdotjs.ZipWriter(createWriter(cfg.outputType), {
-		level: cfg.level,
-		keepOrder: cfg.keepOrder,
-		bufferedWrite: cfg.bufferedWrite
-	});
-	await Promise.all(inputs.map(input => zipWriter.add(input.name, createReader(input.data), {})));
-	const zipped = await zipWriter.close();
-	
-	if (cfg.useWebWorkers) {
-		await zipdotjs.terminateWorkers();
-	}
-	return zipped;
+  const zipWriter = new zipdotjs.ZipWriter(createWriter(cfg.outputType), {
+    level: cfg.level,
+    keepOrder: cfg.keepOrder,
+    bufferedWrite: cfg.bufferedWrite,
+  });
+  await Promise.all(
+    inputs.map((input) =>
+      zipWriter.add(input.name, createReader(input.data), {}),
+    ),
+  );
+  const zipped = await zipWriter.close();
+
+  if (cfg.useWebWorkers) {
+    await zipdotjs.terminateWorkers();
+  }
+  return zipped;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -119,17 +125,21 @@ async function zip(cfg, inputs) {
  * @param {Uint8Array|Blob} zipped
  */
 async function unzip(cfg, zipped) {
-	configure(cfg);
-	const zipReader = new zipdotjs.ZipReader(createReader(zipped), {
-		// interesting options for ZipReader ctr?
-	});
-	const entries = await zipReader.getEntries();
-	const proms = entries.map((entry) => entry.directory ? Promise.resolve(null) : entry.getData(createWriter(cfg.outputType)));
-	const outputs = await Promise.all(proms);
-	if (cfg.useWebWorkers) {
-		await zipdotjs.terminateWorkers();
-	}
-	return outputs;
+  configure(cfg);
+  const zipReader = new zipdotjs.ZipReader(createReader(zipped), {
+    // interesting options for ZipReader ctr?
+  });
+  const entries = await zipReader.getEntries();
+  const proms = entries.map((entry) =>
+    entry.directory
+      ? Promise.resolve(null)
+      : entry.getData(createWriter(cfg.outputType)),
+  );
+  const outputs = await Promise.all(proms);
+  if (cfg.useWebWorkers) {
+    await zipdotjs.terminateWorkers();
+  }
+  return outputs;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -137,12 +147,12 @@ async function unzip(cfg, zipped) {
  * @param {zipdotjs.Configuration} cfg
  */
 function configure(cfg) {
-	zipdotjs.configure({
-		useWebWorkers: cfg.useWebWorkers,
-		maxWorkers: cfg.maxWorkers,
-		useCompressionStream: cfg.useCompressionStream,
-		chunkSize: cfg.chunkSize
-	});
+  zipdotjs.configure({
+    useWebWorkers: cfg.useWebWorkers,
+    maxWorkers: cfg.maxWorkers,
+    useCompressionStream: cfg.useCompressionStream,
+    chunkSize: cfg.chunkSize,
+  });
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -152,15 +162,15 @@ function configure(cfg) {
  * @returns {Uint8Array|Blob} the data containing random bytes.
  */
 function getBlobOrU8(size, inputType) {
-	const data = new Float64Array(Math.floor(size / 8));
-	for (let indexData = 0; indexData < data.length; indexData++) {
-		data[indexData] = Math.random();
-	}
-	if (inputType === "Blob") {
-		return new Blob([data]);
-	} else {
-		return new Uint8Array(data.buffer);
-	}
+  const data = new Float64Array(Math.floor(size / 8));
+  for (let indexData = 0; indexData < data.length; indexData++) {
+    data[indexData] = Math.random();
+  }
+  if (inputType === "Blob") {
+    return new Blob([data]);
+  } else {
+    return new Uint8Array(data.buffer);
+  }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -168,13 +178,13 @@ function getBlobOrU8(size, inputType) {
  * @param {Uint8Array|Blob} data
  */
 function createReader(data) {
-	if (data instanceof Uint8Array) {
-		return new zipdotjs.Uint8ArrayReader(data);
-	} else if (data instanceof Blob) {
-		return new zipdotjs.BlobReader(data);
-	} else {
-		throw new Error();
-	}
+  if (data instanceof Uint8Array) {
+    return new zipdotjs.Uint8ArrayReader(data);
+  } else if (data instanceof Blob) {
+    return new zipdotjs.BlobReader(data);
+  } else {
+    throw new Error();
+  }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -183,9 +193,9 @@ function createReader(data) {
  * @returns {zipdotjs.Uint8ArrayWriter|zipdotjs.BlobWriter} the writer.
  */
 function createWriter(outputType) {
-	 if (outputType === "Blob") {
-		return new zipdotjs.BlobWriter("application/octet-stream");
-	} else {
-		return new zipdotjs.Uint8ArrayWriter();
-	}
+  if (outputType === "Blob") {
+    return new zipdotjs.BlobWriter("application/octet-stream");
+  } else {
+    return new zipdotjs.Uint8ArrayWriter();
+  }
 }
